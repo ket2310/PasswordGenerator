92003bdf565a9721becaf5c365bf4dcef8ef6e40
Kirk Thomas <dablumaroon@gmail.com>
2021-05-12 12:19:08 -0400

    Created prompts for password per criteria.

--- script.js
+++ script.js
@@ -16,26 +16,56 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
 /* 
   GIVEN I need a new, secure password
-     WHEN I click the button to generate a password
+   1  WHEN I click the button to generate a password
      THEN I am presented with a series of prompts for password criteria
      
-     WHEN prompted for password criteria
+   2  WHEN prompted for password criteria
      THEN I select which criteria to include in the password
 */
-   var oneUppercase = confirm("Include one uppercase letter in password?");
-   var oneLowercase = confirm("Include one lowercase letter in password?");
-   var hasNumbers = confirm("Include numbers in password?");
-   var hasSymbols = confirm("Include at least one symbol in password (ex: @,$,&,^,*,");
+   var criteria = ["Include uppercase letters in password?",
+                   "Include lowercase letters in password?",
+                   "Include numbers in password?",
+                   "Include at least one symbol in password (ex: @,$,&,^,*)"];
+
 
 /*
-   WHEN prompted for the length of the password
+  3 WHEN prompted for the length of the password
    THEN I choose a length of at least 8 characters and no more than 128 characters
 */
 
-    var pwLength = prompt("Enter the desired length of the password (must be at least 8 characters).");
-    if (pwLength < 8)
-        alert("password length is too short.  Must be at least 8 characters");
-        
+var pwLength = prompt("Enter the desired length of the password (must be at least 8 characters).");
+while (pwLength < 8){
+   alert("password length is too short.  Must be at least 8 characters");
+   pwLength = prompt("Enter the desired length of the password (must be at least 8 characters).");
+}
 
+
+/*
+  4 WHEN prompted for character types to include in the password
+   THEN I choose lowercase, uppercase, numeric, and/or special characters
+*/
+   var makePassword = new Array();
+   for (var i = 0; i < criteria.length; i++) {
+      makePassword[i] = confirm(criteria[i]);
+   }
+
+ /*
+  5 WHEN I answer each prompt
+   THEN my input should be validated and at least one character type should be 
+   selected
+*/       
+   for (var i = 0; i < makePassword.length; i++)
+   {
+      if (!makePassword[i])
+      {
+         alert("You must choose at least one character type to generate your password.");
+         break;
+      }
+      else
+      {
+         alert("Generating password.  Click Ok to see the result.")
+         break;
+      }
+   }
 
 }
